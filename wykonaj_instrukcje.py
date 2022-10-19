e9e3e6b88083107eea174a9af8f6ea72378afd5d
Rabuuwu <59016101+Rabuuwu@users.noreply.github.com>
2022-10-19 15:33:14 +0200

    Update wykonaj_instrukcje.py

--- wykonaj_instrukcje.py
+++ wykonaj_instrukcje.py
@@ -1,41 +1,53 @@
 tablica = []
+#dlugosc =0
+#komenda = ""
+#pkomenda = ""
 
 plik =  open('instrukcje.txt', 'r').read()
 linijki = plik.split('\n')
-for line in linijki:
-        print(tablica)
+
+def USUN_1():
+    tablica.pop(len(tablica) - 1)
+
+def DOPISZ(dp_lit):
+    tablica.append(dp_lit)
+
+def ZMIEN(zm_lit):
+    tablica.pop(len(tablica) - 1)
+    tablica.append(zm_lit)
+
+def PRZESUN(pr_lit):
+    x = tablica.index(pr_lit)
+    if ord(tablica[x]) == 90:
+        z = chr(65)
+        tablica.pop(x)
+        tablica.insert(x, z)
+    else:
+        z = chr(ord(tablica[x]) + 1)
+        tablica.pop(x)
+        tablica.insert(x, z)
+
+# def LICZ():          #zrobić to w funkcji
+#     dlugosc
+
+try:
+    for line in linijki:
         podzial = line.split()
-
-        def USUN_1():
-            tablica.pop(len(tablica) - 1)
-    
-        def DOPISZ(dp_lit):
-            tablica.append(dp_lit)
-
-        def ZMIEN(zm_lit):
-            tablica.pop(len(tablica) - 1)
-            tablica.append(zm_lit)
-    
-        def PRZESUN(pr_lit):
-             x = tablica.index(pr_lit)
-            if ord(tablica[x]) == 90:
-                z = chr(65)
-                tablica.pop(x)
-                tablica.insert(x, z)
-            else:
-                z = chr(ord(tablica[x]) + 1)
-                tablica.pop(x)
-                tablica.insert(x, z)
-
-
         if podzial[0] == "DOPISZ":
             DOPISZ(podzial[1])
-            
+            komenda = "dopisz"
         elif podzial[0] == "ZMIEN":
             ZMIEN(podzial[1])
-            
+            komenda = "zmien"
         elif podzial[0] == "PRZESUN":
             PRZESUN(podzial[1])
-            
+            komenda = "przesun"
         else:   
             USUN_1()
+            komenda = "usun"
+
+except:
+    print(tablica)
+    print(len(tablica)) #4.1
+    # print(dlugosc) #4.2
+
