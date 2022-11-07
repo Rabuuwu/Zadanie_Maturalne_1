b0587d9c7040715539136980c45208cb60850e41
Rabuuwu <59016101+Rabuuwu@users.noreply.github.com>
2022-11-07 09:00:16 +0100

    update

---
+++ zadanie 2017/piksele.py
@@ -0,0 +1,18 @@
+plik = open("przyklad.txt",'r').read()
+linijki = plik.split()
+x = 0
+
+def linja:
+    for x in range(len(line)):
+        if line[x] == 255:
+            print(line[x])
+        elif line[x] == 0:
+            print(line[x])
+        x+=1
+
+
+
+
+for line in linijki:
+
+
