f28d3768c3e7eb0342cebec13747897a1b6320d5
Rabuuwu <59016101+Rabuuwu@users.noreply.github.com>
2022-11-07 20:28:04 +0100

    Update piksele.py

--- zadanie 2017/piksele.py
+++ zadanie 2017/piksele.py
@@ -1,16 +1,26 @@
-plik = open("dane.txt",'r').read().split()
-piksele = [eval(i) for i in plik]
-print(min(piksele))
-print(max(piksele)) #6.1
+#           Zadanie 6.1
 
-# x = -1
-# z=0
-# a=255
+# plik = open("przyklad.txt",'r').read().split()
+# piksele = [eval(i) for i in plik]
+# print(min(piksele))
+# print(max(piksele)) #6.1
+
+#           Zadanie 6.2
+
+# plik = open("przyklad.txt",'r').read()
+# linijki = plik.split('\n')
+# flag = 0
+# y = 0 
 # for line in linijki:
-#     x+=1
 #     podzial = line.split()
-#     if int(a)>int(podzial[0]):
-#         a = int(podzial[0])
-#     elif int(z)<int(podzial[0]):
-#         z=int(podzial[0])
-# print(a, z)         #6.1
+#     y += 1
+#     x = 0
+#     while x in range((len(podzial))//2):
+#         if podzial[x] != podzial[len(podzial)-1-x]:
+#             flag += 1
+#             break
+#         x += 1
+# print(flag)
+
+#           Zadanie 6.3
+
