78fb17d0b056c730283b2ffd82ee131f1c7d1b5c
Rabuuwu <59016101+Rabuuwu@users.noreply.github.com>
2022-11-19 23:23:11 +0100

    prace nad 4.4

--- zadanie 2022/czerwiec/odbicia.py
+++ zadanie 2022/czerwiec/odbicia.py
@@ -1,6 +1,6 @@
 import math
 
-plik =  open('liczby.txt', 'r').read()
+plik =  open('zadanie 2022\czerwiec\liczby.txt', 'r').read()
 linijki = plik.split('\n')
 roznica = []
 
@@ -18,9 +18,33 @@ def odejmowanie(): #4.2
     liczba3 = line[::-1]
     liczba3 = int(liczba3)
     wynik = abs(liczba2-liczba3)
-    
     roznica.append(wynik)
-    
+
+def czy_pierwsza(n, n2):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+        for i in range(2, n2):
+            if n2 % i == 0:
+                return False
+    print(n)
+
+def odpal_czy_pierwsza():
+    for line in linijki:
+            if line == "":
+                break
+            else:
+                czy_pierwsza(int(line), int(line[::-1]))
+
+def maximum(): #4.2
+    index = roznica.index(max(roznica))
+    print("Max: " + str(linijki[index]) +" "+  str(max(roznica)))
+
+# def liczenie(): #4.4 policzenie roznych, powtarzających sie 2 razy, powt 3  razy
+
+#     print(rozne,duo,trio)
+
+
 
 def main():
     global line
@@ -31,5 +55,18 @@ def main():
             odbicie()
             odejmowanie()
             
-main()
-print("Max: " + str(max(roznica)))
+def skrypt():
+    print("---------4.1---------")            
+    main()
+    print("---------4.2---------")
+    maximum()
+    print("---------4.3---------")
+    odpal_czy_pierwsza()
+    print("---------4.4---------")
+    print("Work in progress...")
+    # liczenie()
+    print("---------------------")
+
+
+skrypt()
+
