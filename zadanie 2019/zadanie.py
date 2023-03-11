f2172c1270134df683e7c512b0027117056f4469
Rabuuwu <59016101+Rabuuwu@users.noreply.github.com>
2023-03-11 13:10:39 +0100

    update

--- zadanie 2019/zadanie.py
+++ zadanie 2019/zadanie.py
@@ -1,9 +1,23 @@
 import math
-plik = open("zadanie 2019\liczby.txt",'r').read()
+plik = open("zadanie 2019\przyklad.txt",'r').read()
 linijki = plik.split()
+
+potegi=[]
+
+# ------------Zadanie 4.1 ------------------
+def is_power_of(number, base):
+  # Base case: when number is smaller than base.
+  if number < base:
+    # If number is equal to 1, it's a power (base**0).
+    if number == 1:
+      return True
+
+    else:return False
+  # Recursive case: keep dividing number by base.
+  return is_power_of(number/base , base)
+
 # ------------Zadanie 4.2 ------------------
 def silnia(n): return n*silnia(n-1) if n > 1 else 1
-
 def rozdziel(n):
     global lista
     global lista2
@@ -17,7 +31,12 @@ def rozdziel(n):
         if a==n:
             print(n)
 print("------------Zadanie 4.2 ------------------")
+
 for i in linijki:
     n=int(i)
+    if(is_power_of(n,3) == True):
+        potegi.append(n)
     rozdziel(n)
-    
+
+print("------------Zadanie 4.1 ------------------")
+print(potegi)
