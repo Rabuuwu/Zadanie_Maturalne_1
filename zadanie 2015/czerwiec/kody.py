1bc80c971ef35ece7527ca7a59cc192ee53b37fc
Rabuuwu <59016101+Rabuuwu@users.noreply.github.com>
2023-03-13 10:24:34 +0100

    wololo

---
+++ zadanie 2015/czerwiec/kody.py
@@ -0,0 +1,24 @@
+plik = open("zadanie 2015\czerwiec\kody.txt",'r').read()
+linijki = plik.split()
+lista=[]
+kod=""
+start=11011010
+stop=11010110
+# for i in linijki:
+#     liczby=list(str(i))
+#     for j in range(len(liczby)-1):
+#         if j%2==0:
+#             lista.append(liczby[j])
+#                print (lista)
+
+
+# 0 10101110111010 
+# 1 11101010101110 
+# 2 10111010101110 
+# 3 11101110101010 
+# 4 10101110101110 
+# 5 11101011101010 
+# 6 10111011101010 
+# 7 10101011101110 
+# 8 11101010111010 
+# 9 10111010111010
