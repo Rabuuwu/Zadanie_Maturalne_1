a27412c82a80f049ace536bd4a4b3b4d2664208b
Rabuuwu <59016101+Rabuuwu@users.noreply.github.com>
2023-03-17 12:14:18 +0100

    wololo

--- zadanie 2019/maj/zadanie.py
+++ zadanie 2019/maj/zadanie.py
@@ -1,7 +1,8 @@
 import math
 plik = open("zadanie 2019\maj\przyklad.txt",'r').read()
 linijki = plik.split()
-
+nwd=[1]
+liczby=[]
 potegi=[]
 
 # ------------Zadanie 4.1 ------------------
@@ -41,4 +42,41 @@ for i in linijki:
 print("------------Zadanie 4.1 ------------------")
 print(potegi)
 
+print("------------Zadanie 4.3 ------------------")
+# ------------Zadanie 4.3 ------------------
+for x in range(len(linijki)):
+  if x!=0:
+     if x!=(len(linijki)-1):
+        liczba_1 = int(linijki[x-1])
+        liczba_2 = int(linijki[x])
+        liczba_3 = int(linijki[x+1])
+        z=math.gcd(liczba_1,liczba_2,liczba_3)
+        if (z!=nwd[0] or z>=nwd[0]) and (z>1 and z>=nwd[0]):  
+          nwd.clear()
+          if len(liczby)>2:
+             liczby.remove(liczby[0])
+          nwd.append(z), liczby.extend([liczba_1,liczba_2,liczba_3])
+     if x==(len(linijki)-1):
+        liczba_1 = int(linijki[x-2])
+        liczba_2 = int(linijki[x-1])
+        liczba_3 = int(linijki[x])
+        z=math.gcd(liczba_1,liczba_2,liczba_3)
+        if (z!=nwd[0] and z>nwd[0]) and (z>1 and z>=nwd[0]):  
+          nwd.clear()
+          liczby.remove(liczby[0])
+          liczby.remove(liczby[0])
+          nwd.append(z), liczby.extend([liczba_1,liczba_2,liczba_3])
+liczby.remove(liczby[0])
+liczby.remove(liczby[0])
+output = []
+for g in liczby:
+    if g not in output:
+        output.append(g)
+ciag=[]
+for h in range (len(output)):
+   if int(output[h])%int(nwd[0])==0:
+      ciag.append(output[h])
 
+print(ciag[0])
+print(len(ciag))
+print(nwd)
