103d1aa211d9d74b1306f836fa90acfaae99a122
Rabuuwu <59016101+Rabuuwu@users.noreply.github.com>
2023-03-29 13:50:41 +0200

    koeljna matura

--- zadanie 2018/czerwiec/zadanie.py
+++ zadanie 2018/czerwiec/zadanie.py
@@ -11,51 +11,51 @@ git2=0
 i=0
 
 # ---------------Zadanie 4.1---------------
-# with open("zadanie 2018/czerwiec/przyklad1.txt",'r') as f:
-#     for line in f:
-#         linijka1 = list(line.split())
-#         ostatnie1.append(linijka1[len(linijka1)-1])
-#         for i in linijka1:
-#             if int(i)%2==0:
-#                 parzyste1+=1
-#             else: nieparzyste1+=1
-#         if parzyste1==nieparzyste1==5:
-#             git1+=1
-#             parzyste1=nieparzyste1=0
+with open("zadanie 2018/czerwiec/przyklad1.txt",'r') as f:
+    for line in f:
+        linijka1 = list(line.split())
+        ostatnie1.append(linijka1[len(linijka1)-1])
+        for i in linijka1:
+            if int(i)%2==0:
+                parzyste1+=1
+            else: nieparzyste1+=1
+        if parzyste1==nieparzyste1==5:
+            git1+=1
+            parzyste1=nieparzyste1=0
 
-# with open("zadanie 2018/czerwiec/przyklad2.txt",'r') as f:
-#     for line in f:
-#         linijka2 = list(line.split())
-#         ostatnie2.append(linijka2[len(linijka2)-1])
-#         for i in linijka2:
-#             if int(i)%2==0:
-#                 parzyste2+=1
-#             else: nieparzyste2+=1
+with open("zadanie 2018/czerwiec/przyklad2.txt",'r') as f:
+    for line in f:
+        linijka2 = list(line.split())
+        ostatnie2.append(linijka2[len(linijka2)-1])
+        for i in linijka2:
+            if int(i)%2==0:
+                parzyste2+=1
+            else: nieparzyste2+=1
 
-#         if parzyste2==nieparzyste2==5:
-#             git2+=1
-#             parzyste2=nieparzyste2=0
+        if parzyste2==nieparzyste2==5:
+            git2+=1
+            parzyste2=nieparzyste2=0
 
-# for i in range(len(ostatnie1)):
-#     if(ostatnie1[i]==ostatnie2[i]):
-#         podobne+=1
-# print("---------------Zadanie 4.1---------------")
-# print(podobne)
+for i in range(len(ostatnie1)):
+    if(ostatnie1[i]==ostatnie2[i]):
+        podobne+=1
+print("---------------Zadanie 4.1---------------")
+print(podobne)
 
-# # ---------------Zadanie 4.2---------------
-# print("---------------Zadanie 4.2---------------")
-# pary = (git2+git1)/2
-# print(pary)
+# ---------------Zadanie 4.2---------------
+print("---------------Zadanie 4.2---------------")
+pary = (git2+git1)/2
+print(pary)
 
 # ---------------Zadanie 4.3---------------
-print("---------------Zadanie 4.3---------------")
-count=0
-def licz_te_same(plik1,plik2):
-    with open(plik1, 'r') as f1, open(plik2, 'r') as f2:
+# print("---------------Zadanie 4.3---------------")
+# count=0
+# def licz_te_same(plik1,plik2):
+#     with open(plik1, 'r') as f1, open(plik2, 'r') as f2:
         
                         
-    # return count
-# print(licz_te_same("zadanie 2018/czerwiec/przyklad1.txt","zadanie 2018/czerwiec/przyklad2.txt"))
+#     # return count
+# # print(licz_te_same("zadanie 2018/czerwiec/przyklad1.txt","zadanie 2018/czerwiec/przyklad2.txt"))
 
 
 
--- zadanie 2021/czerwiec/napisy.py
+++ zadanie 2021/czerwiec/napisy.py
@@ -1,2 +1,20 @@
-plik = open("zadanie 2021\czerwiec\napisy.txt",'r').read()
-linijki = plik.split('/n')
+liczby=0
+i=0
+napis=""
+a=0
+b=1
+l = ['0','1','2','3','4','5','6','7','8','9']
+with open("zadanie 2021/czerwiec/przyklad.txt",'r') as file:
+    for line in file:
+        linijka = [char for char in line]
+        if b%20==0:
+            if a < 50:
+                napis=napis+linijka[a]
+                a+=1
+        for i in range(len(linijka)):
+            if linijka[i] in l:
+                liczby+=1
+        b+=1
+
+print(liczby)
+print (napis)
