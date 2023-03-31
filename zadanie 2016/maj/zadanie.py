0fe46552031051e4728fdbc9f043598cc3f8d428
Rabuuwu <59016101+Rabuuwu@users.noreply.github.com>
2023-03-31 11:32:03 +0200

    bruh

--- zadanie 2016/maj/zadanie.py
+++ zadanie 2016/maj/zadanie.py
@@ -1,4 +1,28 @@
-with open("zadanie 2016/maj/punkty.txt", "r") as f:
+import math
+
+litery = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K','L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+kod = []
+k=107
+
+# with open("zadanie 2016/maj/dane_6_1.txt", "r") as f:
+#     for line in f:
+#         linia = line.split()
+#         wiersz = str(linia[0])
+#         literki = list(wiersz)
+#         i=0
+#         for i in range(len(literki)):
+#             x= litery.index(literki[i])
+#             y=x+k
+#             z=y%26
+#             kod.append(litery[z])
+#         print(kod)
+#         kod.clear()
+
+with open("zadanie 2016/maj/dane_6_2.txt", "r") as f:
     for line in f:
-        kordy = line.split()
-        print(kordy)
+        linia = line.split()
+        wiersz = str(linia[0])
+        l=int(linia[1])
+        literki = list(wiersz)
+        i=0
+        
